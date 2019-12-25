7c0a4e6932f7dbc3436fcbca58f8a0d2166b59ad
OUARDI Yakoub Abdallah <ouardi.yakoubabdallah@gmail.com>
2019-12-26 00:29:19 +0100

    The assistant can now speak to us

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import speech_recognition as sr
 from time import ctime
+from random import randint,random
+from gtts import gTTS
+import speech_recognition as sr
 import webbrowser as wb
-from random import randint
+import playsound as player
+import os
+
 
 r= sr.Recognizer()
 bot_name = 'Sara'
@@ -9,45 +13,54 @@ bot_name = 'Sara'
 def record_audio(ask = None):
     with sr.Microphone() as src :
         if ask is not None:
-            print(ask)
+            speak(ask)
         audio_input = r.listen(src)
         text_data = ''
         try:
             text_data = r.recognize_google(audio_input)
         except sr.UnknownValueError as e1:
             print(e1.with_traceback())
-            print('Oups, i didn\'t hear you corectly :( ...')
+            speak('Oups, i didn\'t hear you corectly :( ...')
         except sr.RequestError as e2:
             print(e2.with_traceback())
-            print('Sorry, the service is currently down')
+            speak('Sorry, the service is currently down')
         return text_data
 
 def respond(data):
     if 'What is your name'.lower() in data.lower():
-        print('My name is {}'.format(bot_name))
+        speak('My name is {}'.format(bot_name))
     elif 'What time is it'.lower() in data.lower():
-        print(ctime())
+        speak(ctime())
     #searching for something in google
     elif 'Search'.lower() in data.lower():
         q = record_audio('What do you want to search for')
         google_search_url = 'https://www.google.com/search?q={}'.format(q)
         wb.get().open(google_search_url)
-        print('Here is what i found about {}'.format(q))
+        speak('Here is what i found about {}'.format(q))
     elif 'Find location'.lower() in data.lower():
         l = record_audio('What is the place you want to locate')
         google_search_url = 'https://www.google.com/maps/place/{}/&amp'.format(l)
         wb.get().open(google_search_url)
-        print('Here is the location of {}'.format(l))
+        speak('Here is the location of {}'.format(l))
     elif 'exit'.lower() or 'bye'.lower() or 'goodbye'.lower() in data.lower():
         #say goodbye randomly each time
         farwell_expressions = ['See you later', 'Ok, goodbye', 'ByeBye']
         n = randint(0,len(farwell_expressions)-1)
-        print(farwell_expressions[n])
+        speak(farwell_expressions[n])
         exit()
     else:
-        print('You said: {}'.format(text_data))
+        speak('You said: {}'.format(text_data))
 
-print('Listening...')
+def speak(sentence):
+    text_to_speech = gTTS(text=sentence, lang='en')
+    r = randint(1,100000000)
+    audio_file = 'aud-{}.mp3'.format(r)
+    text_to_speech.save(audio_file)
+    player.playsound(audio_file)
+    print(sentence)
+    os.remove(audio_file)
+
+speak('Listening...')
 
 while True:
 
